8791279fef8cd1ea0449a15695b0f81410333a1f
Ranjith K <ranjith.masthikatte@gmail.com>
2021-05-07 00:04:57 +0200

    Update LoginController.java

--- demo-project/src/main/java/de/fraunhofer/iem/secucheck/todolist/controllers/LoginController.java
+++ demo-project/src/main/java/de/fraunhofer/iem/secucheck/todolist/controllers/LoginController.java
@@ -1,22 +1,18 @@
 package de.fraunhofer.iem.secucheck.todolist.controllers;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-
+import de.fraunhofer.iem.secucheck.todolist.model.User;
+import de.fraunhofer.iem.secucheck.todolist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import de.fraunhofer.iem.secucheck.todolist.model.User;
-import de.fraunhofer.iem.secucheck.todolist.service.UserService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
 
 @Controller
 public class LoginController {
