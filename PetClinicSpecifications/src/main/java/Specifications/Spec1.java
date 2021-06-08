d1a32ffb97cd231838d43c76dd3631ad3d2aca72
ranjithk <ranjith.masthikatte@gmail.com>
2021-06-09 01:47:00 +0200

    Added specifications for the PetClinic

---
+++ PetClinicSpecifications/src/main/java/Specifications/Spec1.java
@@ -0,0 +1,45 @@
+package Specifications;
+
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.CONSTANTS.LOCATION;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.MethodSelector;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.TaintFlowQueryBuilder;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.annotations.*;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.FluentTQLSpecification;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.MethodPackage.Method;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.Query.TaintFlowQuery;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.SpecificationInterface.FluentTQLUserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FluentTQLSpecificationClass
+public class Spec1 implements FluentTQLUserInterface {
+    @OutFlowParam(parameterID = {0})
+    public Method source = new MethodSelector("org.springframework.samples.petclinic.owner.OwnerController: " +
+            "org.springframework.web.servlet.ModelAndView showOwner(int)");
+
+    @InFlowParam(parameterID = {0})
+    @OutFlowReturnValue
+    public Method requiredProp = new MethodSelector("javax.persistence.EntityManager: " +
+            "javax.persistence.TypedQuery createQuery(java.lang.String,java.lang.Class)");
+
+    @InFlowThisObject
+    public Method sink = new MethodSelector("javax.persistence.TypedQuery: " +
+            "java.lang.Object getSingleResult()");
+
+    @Override
+    public List<FluentTQLSpecification> getFluentTQLSpecification() {
+        TaintFlowQuery taintFlow = new TaintFlowQueryBuilder("h1")
+                .from(source)
+                .through(requiredProp)
+                .to(sink)
+                .report("H-Injection 1")
+                .at(LOCATION.SOURCEANDSINK)
+                .build();
+
+        List<FluentTQLSpecification> myFluentTQLSpecs = new ArrayList<FluentTQLSpecification>();
+        myFluentTQLSpecs.add(taintFlow);
+
+        return myFluentTQLSpecs;
+    }
+}
---
+++ PetClinicSpecifications/src/main/java/Specifications/Spec3.java
@@ -0,0 +1,53 @@
+package Specifications;
+
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.CONSTANTS.LOCATION;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.MethodSelector;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.TaintFlowQueryBuilder;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.annotations.*;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.FluentTQLSpecification;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.MethodPackage.Method;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.Query.TaintFlowQuery;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.SpecificationInterface.FluentTQLUserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FluentTQLSpecificationClass
+public class Spec3 implements FluentTQLUserInterface {
+    @OutFlowParam(parameterID = {0})
+    public Method source = new MethodSelector("org.springframework.samples.petclinic.owner.OwnerController: " +
+            "java.lang.String processFindForm(" +
+            "org.springframework.samples.petclinic.owner.Owner," +
+            "org.springframework.validation.BindingResult," +
+            "java.util.Map)");
+
+    @InFlowParam(parameterID = {0})
+    @OutFlowReturnValue
+    public Method requiredProp = new MethodSelector("javax.persistence.EntityManager: " +
+            "javax.persistence.TypedQuery createQuery(java.lang.String,java.lang.Class)");
+
+    @InFlowThisObject
+    @OutFlowReturnValue
+    public Method requiredProp1 = new MethodSelector("org.springframework.samples.petclinic.model.Person: " +
+            "java.lang.String getLastName()");
+
+    @InFlowThisObject
+    public Method sink = new MethodSelector("javax.persistence.TypedQuery: java.util.List getResultList()");
+
+    @Override
+    public List<FluentTQLSpecification> getFluentTQLSpecification() {
+        TaintFlowQuery taintFlow = new TaintFlowQueryBuilder("h3")
+                .from(source)
+                .through(requiredProp1)
+                .through(requiredProp)
+                .to(sink)
+                .report("H-Injection 3")
+                .at(LOCATION.SOURCEANDSINK)
+                .build();
+
+        List<FluentTQLSpecification> myFluentTQLSpecs = new ArrayList<FluentTQLSpecification>();
+        myFluentTQLSpecs.add(taintFlow);
+
+        return myFluentTQLSpecs;
+    }
+}
