921322b3a170ecd0826f4af3988232b0205e5be8
ranjithkris <ranjith.masthikatte@gmail.com>
2024-03-19 16:48:41 +0100

    Specification for BenchmarkTest00026 from OWASP

---
+++ OwaspSpecification/src/main/java/Specification/Benchmark00026Spec.java
@@ -0,0 +1,43 @@
+package Specification;
+
+import de.fraunhofer.iem.secucheck.fluenttql.dsl.CONSTANTS.LOCATION;
+import de.fraunhofer.iem.secucheck.fluenttql.dsl.MethodConfigurator;
+import de.fraunhofer.iem.secucheck.fluenttql.dsl.TaintFlowQueryBuilder;
+import de.fraunhofer.iem.secucheck.fluenttql.dsl.annotations.FluentTQLSpecificationClass;
+import de.fraunhofer.iem.secucheck.fluenttql.interfaces.FluentTQLSpecification;
+import de.fraunhofer.iem.secucheck.fluenttql.interfaces.MethodPackage.Method;
+import de.fraunhofer.iem.secucheck.fluenttql.interfaces.Query.TaintFlowQuery;
+import de.fraunhofer.iem.secucheck.fluenttql.interfaces.SpecificationInterface.FluentTQLUserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FluentTQLSpecificationClass
+public class Benchmark00026Spec implements FluentTQLUserInterface {
+    public Method source = new MethodConfigurator(
+            "javax.servlet.http.HttpServletRequest: " +
+                    "java.lang.String " +
+                    "getParameter(java.lang.String)")
+            .out().returnValue()
+            .configure();
+
+    public Method sink = new MethodConfigurator(
+            "org.springframework.jdbc.core.JdbcTemplate: " +
+                    "org.springframework.jdbc.support.rowset.SqlRowSet queryForRowSet(java.lang.String)")
+            .in().param(0)
+            .configure();
+
+    public List<FluentTQLSpecification> getFluentTQLSpecification() {
+        TaintFlowQuery taintFlow1 = new TaintFlowQueryBuilder("Benchmark00026")
+                .from(source)
+                .to(sink)
+                .report("Benchmark00026 SQLi")
+                .at(LOCATION.SOURCEANDSINK)
+                .build();
+
+        List<FluentTQLSpecification> myFluentTQLSpecs = new ArrayList<FluentTQLSpecification>();
+        myFluentTQLSpecs.add(taintFlow1);
+
+        return myFluentTQLSpecs;
+    }
+}
