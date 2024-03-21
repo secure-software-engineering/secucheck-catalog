157117b4f0825edb5cd79b7f5b2abea4844a6d5d
ranjithkris <ranjith.masthikatte@gmail.com>
2024-03-21 16:47:40 +0100

    Specification for Benchmark00033 from OWASP

---
+++ OwaspSpecification/src/main/java/Specification/Benchmark00033Spec.java
@@ -0,0 +1,32 @@
+package Specification;
+
+import Specification.FluentTQLRepositories.GeneralPropagators.Props;
+import Specification.FluentTQLRepositories.GeneralPropagators.Sinks;
+import Specification.FluentTQLRepositories.GeneralPropagators.Sources;
+import de.fraunhofer.iem.secucheck.fluenttql.dsl.CONSTANTS.LOCATION;
+import de.fraunhofer.iem.secucheck.fluenttql.dsl.TaintFlowQueryBuilder;
+import de.fraunhofer.iem.secucheck.fluenttql.dsl.annotations.FluentTQLSpecificationClass;
+import de.fraunhofer.iem.secucheck.fluenttql.interfaces.FluentTQLSpecification;
+import de.fraunhofer.iem.secucheck.fluenttql.interfaces.Query.TaintFlowQuery;
+import de.fraunhofer.iem.secucheck.fluenttql.interfaces.SpecificationInterface.FluentTQLUserInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FluentTQLSpecificationClass
+public class Benchmark00033Spec implements FluentTQLUserInterface {
+    public List<FluentTQLSpecification> getFluentTQLSpecification() {
+        TaintFlowQuery taintFlow1 = new TaintFlowQueryBuilder("Benchmark00033")
+                .from(Sources.source_00032)
+                .through(Props.prop1_00032)
+                .to(Sinks.sink_00026)
+                .report("Benchmark00033 SQLi")
+                .at(LOCATION.SOURCEANDSINK)
+                .build();
+
+        List<FluentTQLSpecification> myFluentTQLSpecs = new ArrayList<FluentTQLSpecification>();
+        myFluentTQLSpecs.add(taintFlow1);
+
+        return myFluentTQLSpecs;
+    }
+}
